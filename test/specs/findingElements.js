a022b78c7a6cbf7b2bcd1cf474fc4132b16d14f5
Mashruf Rohon <mashrufrohon@gmail.com>
2025-10-27 19:21:23 +0600

    find elements by class

--- test/specs/findingElements.js
+++ test/specs/findingElements.js
@@ -1,5 +1,5 @@
 describe("Find elements",()=>{
-    
+
     it("By accessibility id", async () =>{
 
         //getting phone element by accessibility id
@@ -20,4 +20,35 @@ describe("Find elements",()=>{
         //asserting for more option to be exist
         await expect(moreOption).toBeExisting();
     })
+
+    it.only("By class", async () =>{
+
+        //getting phone element by accessibility id
+        const chrome = await $('~Chrome');
+
+        //wait after getting the element
+        await browser.pause(5000);
+
+        //click the phone
+        await chrome.click();
+
+        //wait after getting the element
+        await browser.pause(5000);
+
+        //getting the search bar by class name
+        const className = await $('android.widget.EditText');
+
+        //getting the text of the placeholder of searchbar
+        const text = await className.getText();
+
+        //printing the placeholder text of the searchbar
+        console.log("Text is "+ text);
+
+        //asserting the text of the placeholder of the searchbar
+        await expect(className).toHaveText('Search or type URL');
+    })
+
 })
+
+
+
