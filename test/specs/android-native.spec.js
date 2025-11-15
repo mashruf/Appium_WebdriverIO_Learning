75bac8cd038e7bbd66f3de1b82704157590bd636
Mashruf Rohon <mashrufrohon@gmail.com>
2025-11-15 22:08:19 +0600

    working with dialog alert

--- test/specs/android-native.spec.js
+++ test/specs/android-native.spec.js
@@ -10,4 +10,27 @@ describe("Android native feature tests",()=>{
         //assertion
         await expect ($('//*[@text="App/Alert Dialogs"]')).toExist();
     })
+
+    it.only("Working with dialog box", async ()=>{
+        //access activity
+        await driver.startActivity("io.appium.android.apis",".app.AlertDialogSamples");
+
+        //click on first dialog
+        await $('~OK Cancel dialog with a message').click();
+
+        //Getting text from the alert box
+        console.log('Alert text -->',await driver.getAlertText());
+
+        //Accept alert
+        // await driver.acceptAlert();
+
+        //Dismiss alert
+        // await driver.dismissAlert();
+
+        //Clicking the ok button of alert box
+        await $('//*[@resource-id="android:id/button1"]').click();
+
+        //assertion- alert box is no longer visible
+        await expect($('//*[@resource-id="android:id/alertTitle"]')).not.toExist();
+    })
 })
