6d3bdacaae601e95b195827e43af87cf9c273e1c
Mashruf Rohon <mashrufrohon@gmail.com>
2025-11-18 13:51:22 +0600

    Exercise delete note

--- test/specs/android/add-note.spec.js
+++ test/specs/android/add-note.spec.js
@@ -37,4 +37,38 @@ describe("Add note",()=>{
 
 
     })
+
+    it("Delete note", async()=>{
+
+        //click on save note
+        await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]')
+        .click();
+
+        //click on options
+        await $('~More').click();
+
+        //click delete
+        await $('//*[@text="Delete"]').click();
+
+        //click ok
+        await $('//*[@text="OK"]').click();
+
+        //assertion saved note is not displayed in home screen
+        await expect($('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]'))
+        .not.toBeDisplayed();
+
+        //click burger button
+        await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/icon_nav"]')
+        .click();
+
+        //go to trash
+        await $('//*[@text="Trash Can"]').click();
+
+        //assertion whether the note is deleted
+        await expect($('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]'))
+        .toBeDisplayed();
+
+        await expect($('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]'))
+        .toHaveText('Favorite superheroes');
+    })
 })
