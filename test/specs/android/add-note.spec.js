c13d0ba822047b15f548278bc6ff6dfc8a2c4d6b
Mashruf Rohon <mashrufrohon@gmail.com>
2025-11-18 13:09:54 +0600

    Add note, save changes and verify note

--- test/specs/android/add-note.spec.js
+++ test/specs/android/add-note.spec.js
@@ -6,4 +6,35 @@ describe("Add note",()=>{
         //assertion
         await expect($('//*[@text="Add note"]')).toBeDisplayed();
     })
+    it("Add note, save changes and verify note", async ()=>{
+        //click add note
+        await $('//*[@text="Add note"]').click();
+
+        //click text
+        await $('//*[@text="Text"]').click();
+
+        //asserton editing should be displayed
+        await expect($('//*[@text="Editing"]')).toBeDisplayed();
+
+        //add title of note
+        await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_title"]')
+        .addValue("Favorite superheroes");
+
+        //Add note
+        await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_note"]')
+        .addValue("Spiderman\nSuperman\nHulk");
+
+        //save note
+        await driver.back();
+        await driver.back();
+
+        //Assertion note is saved
+        await expect($('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]'))
+        .toBeDisplayed();
+
+        await expect($('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]'))
+        .toHaveText('Favorite superheroes');
+
+
+    })
 })
